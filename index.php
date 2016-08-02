2e5212c8f76a58b7b4e1888203c6eb253c8e629d
Jakub Chábek <jakub@chabek.cz>
2016-08-02 21:52:32 +0200

    Fix updating feature
    If version differs then download the new one. Also check if the download was successfull to prevent removing adminer completely.

--- index.php
+++ index.php
@@ -3,10 +3,12 @@
 $actualVersion = file_get_contents('version');
 $newVersion = isset($_COOKIE['adminer_version']) ? $_COOKIE['adminer_version'] : 0;
 
-if (version_compare($actualVersion, $newVersion) == -1) {
+if (version_compare($actualVersion, $newVersion) !== 0) {
 	file_put_contents('version', $newVersion);
 	$data = file_get_contents('https://www.adminer.org/latest-mysql-en.php');
-	file_put_contents('adminer.php', $data);
+	if ($data) {
+		file_put_contents('adminer.php', $data);
+	}
 }
 
 function adminer_object() {
